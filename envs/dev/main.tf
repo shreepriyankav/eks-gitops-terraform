a51c7bdb9f5beb0ffd22be5c320b41dc1a02b872
shreepriyankav <shreepriyanka2004@gmail.com>
2026-09-25 11:46:42 +0530

    Add Argo CD with Helm provider

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -22,3 +22,15 @@ module "eks" {
 
   environment = var.environment
 }
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  wait    = true
+  timeout = 600
+}
--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -6,9 +6,30 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-2"
+}
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
 }
