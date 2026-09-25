01acaef55ec5ba0fc881d66df40e4329df91dd56
shreepriyankav <shreepriyanka2004@gmail.com>
2026-09-25 12:35:55 +0530

    Add Spring Boot application and Docker setup

---
+++ app/src/test/java/com/devops/gitops/GitopsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.gitops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitopsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
